fb4ee4b2243ae71dc046441f1887790799825e88
nik <niksik9024@outlook.com>
2025-08-07 11:40:27 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // This is to allow requests from the cloud development environment.
@@ -23,4 +22,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
