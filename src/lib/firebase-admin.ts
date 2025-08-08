7582dddaf71866ab14b6563872927163019dd8c0
nik <niksik9024@outlook.com>
2025-08-08 08:16:18 +0000

    why i am able to see firebase firestore related files

--- src/lib/firebase-admin.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is intentionally left blank as all Firebase dependencies are being removed.
-// It is kept to prevent import errors in other files that might still reference it temporarily
-// during the migration, but all its content and usage will be removed.
